f21cb95c2a64e2ba1f203cd8579d981e8b6a3e15
Hawk0n <Hsolafoss@gmail.com>
2021-05-15 23:53:00 +0200

    GPU utilization working

--- 3D Cellular Automata/Assets/_Game/_Scripts/Cellular_Logic.cs
+++ 3D Cellular Automata/Assets/_Game/_Scripts/Cellular_Logic.cs
@@ -6,113 +6,113 @@ using Unity.Collections;
 
 public class Cellular_Logic : MonoBehaviour
 {
-    GameBoard gameBoard;
+    // GameBoard gameBoard;
 
-    public void CalculateNextEvolution()
-    {
-        if(gameBoard == null)
-            gameBoard = GetComponent<GameBoard>();
+    // public void CalculateNextEvolution()
+    // {
+    //     if(gameBoard == null)
+    //         gameBoard = GetComponent<GameBoard>();
         
-        int3 centerIndex = new int3(0,0,0);
+    //     int3 centerIndex = new int3(0,0,0);
 
-        for (int i = 0; i < gameBoard.cellCount; i++)
-        {
-            // Find next centerIndex
-            centerIndex = new int3(i / (gameBoard.boardDimensions.z * gameBoard.boardDimensions.y), 
-                                    (i / gameBoard.boardDimensions.z) % gameBoard.boardDimensions.y,
-                                    i % gameBoard.boardDimensions.z);
+    //     for (int i = 0; i < gameBoard.cellCount; i++)
+    //     {
+    //         Find next centerIndex
+    //         centerIndex = new int3(i / (gameBoard.boardDimensions.z * gameBoard.boardDimensions.y), 
+    //                                 (i / gameBoard.boardDimensions.z) % gameBoard.boardDimensions.y,
+    //                                 i % gameBoard.boardDimensions.z);
 
-            // Find all surrounding indexes
-            int3[] surroundingIndexes = 
-            {
-                // Bottom layer
-                new int3(centerIndex.x+0, centerIndex.y-1, centerIndex.z+1), // 1 forward
-                new int3(centerIndex.x+1, centerIndex.y-1, centerIndex.z+1), // 2 forward right
-                new int3(centerIndex.x+1, centerIndex.y-1, centerIndex.z+0), // 3 right
-                new int3(centerIndex.x+1, centerIndex.y-1, centerIndex.z-1), // 4 back right
-                new int3(centerIndex.x-0, centerIndex.y-1, centerIndex.z-1), // 5 back
-                new int3(centerIndex.x-1, centerIndex.y-1, centerIndex.z-1), // 6 back left
-                new int3(centerIndex.x-1, centerIndex.y-1, centerIndex.z-0), // 7 left
-                new int3(centerIndex.x-1, centerIndex.y-1, centerIndex.z+1), // 8 forward left
-                new int3(centerIndex.x+0, centerIndex.y-1, centerIndex.z+0), // 9 underneath
+    //         Find all surrounding indexes
+    //         int3[] surroundingIndexes = 
+    //         {
+    //             Bottom layer
+    //             new int3(centerIndex.x+0, centerIndex.y-1, centerIndex.z+1), // 1 forward
+    //             new int3(centerIndex.x+1, centerIndex.y-1, centerIndex.z+1), // 2 forward right
+    //             new int3(centerIndex.x+1, centerIndex.y-1, centerIndex.z+0), // 3 right
+    //             new int3(centerIndex.x+1, centerIndex.y-1, centerIndex.z-1), // 4 back right
+    //             new int3(centerIndex.x-0, centerIndex.y-1, centerIndex.z-1), // 5 back
+    //             new int3(centerIndex.x-1, centerIndex.y-1, centerIndex.z-1), // 6 back left
+    //             new int3(centerIndex.x-1, centerIndex.y-1, centerIndex.z-0), // 7 left
+    //             new int3(centerIndex.x-1, centerIndex.y-1, centerIndex.z+1), // 8 forward left
+    //             new int3(centerIndex.x+0, centerIndex.y-1, centerIndex.z+0), // 9 underneath
                 
-                // Middle layer
-                new int3(centerIndex.x+0, centerIndex.y+0, centerIndex.z+1), // 10 forward
-                new int3(centerIndex.x+1, centerIndex.y+0, centerIndex.z+1), // 11 forward right
-                new int3(centerIndex.x+1, centerIndex.y+0, centerIndex.z+0), // 12 right
-                new int3(centerIndex.x+1, centerIndex.y+0, centerIndex.z-1), // 13 back right
-                new int3(centerIndex.x-0, centerIndex.y+0, centerIndex.z-1), // 14 back
-                new int3(centerIndex.x-1, centerIndex.y+0, centerIndex.z-1), // 15 back left
-                new int3(centerIndex.x-1, centerIndex.y+0, centerIndex.z-0), // 16 left
-                new int3(centerIndex.x-1, centerIndex.y+0, centerIndex.z+1), // 17 forward left
+    //             Middle layer
+    //             new int3(centerIndex.x+0, centerIndex.y+0, centerIndex.z+1), // 10 forward
+    //             new int3(centerIndex.x+1, centerIndex.y+0, centerIndex.z+1), // 11 forward right
+    //             new int3(centerIndex.x+1, centerIndex.y+0, centerIndex.z+0), // 12 right
+    //             new int3(centerIndex.x+1, centerIndex.y+0, centerIndex.z-1), // 13 back right
+    //             new int3(centerIndex.x-0, centerIndex.y+0, centerIndex.z-1), // 14 back
+    //             new int3(centerIndex.x-1, centerIndex.y+0, centerIndex.z-1), // 15 back left
+    //             new int3(centerIndex.x-1, centerIndex.y+0, centerIndex.z-0), // 16 left
+    //             new int3(centerIndex.x-1, centerIndex.y+0, centerIndex.z+1), // 17 forward left
 
-                // Top layer
-                new int3(centerIndex.x+0, centerIndex.y+1, centerIndex.z+1), // 18 forward
-                new int3(centerIndex.x+1, centerIndex.y+1, centerIndex.z+1), // 19 forward right
-                new int3(centerIndex.x+1, centerIndex.y+1, centerIndex.z+0), // 20 right
-                new int3(centerIndex.x+1, centerIndex.y+1, centerIndex.z-1), // 21 back right
-                new int3(centerIndex.x-0, centerIndex.y+1, centerIndex.z-1), // 22 back
-                new int3(centerIndex.x-1, centerIndex.y+1, centerIndex.z-1), // 23 back left
-                new int3(centerIndex.x-1, centerIndex.y+1, centerIndex.z-0), // 24 left
-                new int3(centerIndex.x-1, centerIndex.y+1, centerIndex.z+1), // 25 forward left
-                new int3(centerIndex.x+0, centerIndex.y+1, centerIndex.z+0), // 26 underneath
-            };
+    //             Top layer
+    //             new int3(centerIndex.x+0, centerIndex.y+1, centerIndex.z+1), // 18 forward
+    //             new int3(centerIndex.x+1, centerIndex.y+1, centerIndex.z+1), // 19 forward right
+    //             new int3(centerIndex.x+1, centerIndex.y+1, centerIndex.z+0), // 20 right
+    //             new int3(centerIndex.x+1, centerIndex.y+1, centerIndex.z-1), // 21 back right
+    //             new int3(centerIndex.x-0, centerIndex.y+1, centerIndex.z-1), // 22 back
+    //             new int3(centerIndex.x-1, centerIndex.y+1, centerIndex.z-1), // 23 back left
+    //             new int3(centerIndex.x-1, centerIndex.y+1, centerIndex.z-0), // 24 left
+    //             new int3(centerIndex.x-1, centerIndex.y+1, centerIndex.z+1), // 25 forward left
+    //             new int3(centerIndex.x+0, centerIndex.y+1, centerIndex.z+0), // 26 underneath
+    //         };
 
-            int cellMap = 0, aliveNeighbourCount = 0;
-            GameBoard.Cell cell = new GameBoard.Cell();
-            for(int k = 0; k < surroundingIndexes.Length; k++)
-            {
-                if(gameBoard.currentCellArray.TryGetValue(surroundingIndexes[k], out cell))
-                {
-                    if(cell.isAlive)
-                    {
-                        aliveNeighbourCount++;
-                        cellMap += (1 << k);
-                    }
-                }
-            }
+    //         int cellMap = 0, aliveNeighbourCount = 0;
+    //         GameBoard.Cell cell = new GameBoard.Cell();
+    //         for(int k = 0; k < surroundingIndexes.Length; k++)
+    //         {
+    //             if(gameBoard.currentCellArray.TryGetValue(surroundingIndexes[k], out cell))
+    //             {
+    //                 if(cell.isAlive)
+    //                 {
+    //                     aliveNeighbourCount++;
+    //                     cellMap += (1 << k);
+    //                 }
+    //             }
+    //         }
 
-            var isAlive = CalculateNextCell(centerIndex, cellMap, aliveNeighbourCount);
-            gameBoard.nextCellArray[centerIndex] = isAlive;
-        }
-    }
+    //         var isAlive = CalculateNextCell(centerIndex, cellMap, aliveNeighbourCount);
+    //         gameBoard.nextCellArray[centerIndex] = isAlive;
+    //     }
+    // }
 
-    GameBoard.Cell CalculateNextCell(int3 centerIndex, int cellMap, int aliveNeighbourCount)
-    {
-        GameBoard.Cell newCell = new GameBoard.Cell();
-        newCell.isAlive = Rules(gameBoard.rule, centerIndex, cellMap, aliveNeighbourCount);
-        return newCell;
-    }
+    // GameBoard.Cell CalculateNextCell(int3 centerIndex, int cellMap, int aliveNeighbourCount)
+    // {
+    //     GameBoard.Cell newCell = new GameBoard.Cell();
+    //     newCell.isAlive = Rules(gameBoard.rule, centerIndex, cellMap, aliveNeighbourCount);
+    //     return newCell;
+    // }
 
-    bool Rules(int rule, int3 centerIndex, int cellMap, int aliveNeighbourCount)
-    {
-        switch (rule)
-        {
-            case 1:
-                if(aliveNeighbourCount <= 7 && gameBoard.currentCellArray[centerIndex].isAlive) {
-                    // Debug.Log("Died from underpopulation");
-                    return false;
-                }
-                else if(aliveNeighbourCount >= 17 && gameBoard.currentCellArray[centerIndex].isAlive) {
-                    // Debug.Log("Died from overpopulation");
-                    return false;
-                }
-                else if(aliveNeighbourCount == 8 && !gameBoard.currentCellArray[centerIndex].isAlive) {
-                    // Debug.Log("Born");
-                    return true;
-                } 
-                else if(gameBoard.currentCellArray[centerIndex].isAlive) {
-                    // Debug.Log("Survived");
-                    return true;
-                }
-                return false;
+    // bool Rules(int rule, int3 centerIndex, int cellMap, int aliveNeighbourCount)
+    // {
+    //     switch (rule)
+    //     {
+    //         case 1:
+    //             if(aliveNeighbourCount <= 7 && gameBoard.currentCellArray[centerIndex].isAlive) {
+    //                 Debug.Log("Died from underpopulation");
+    //                 return false;
+    //             }
+    //             else if(aliveNeighbourCount >= 17 && gameBoard.currentCellArray[centerIndex].isAlive) {
+    //                 Debug.Log("Died from overpopulation");
+    //                 return false;
+    //             }
+    //             else if(aliveNeighbourCount == 8 && !gameBoard.currentCellArray[centerIndex].isAlive) {
+    //                 Debug.Log("Born");
+    //                 return true;
+    //             } 
+    //             else if(gameBoard.currentCellArray[centerIndex].isAlive) {
+    //                 Debug.Log("Survived");
+    //                 return true;
+    //             }
+    //             return false;
 
-            case 2:
-                return false;
+    //         case 2:
+    //             return false;
 
-            default:
-                Debug.LogError("Nonexistent Rule");
-                return false;
-        }
-    }
+    //         default:
+    //             Debug.LogError("Nonexistent Rule");
+    //             return false;
+    //     }
+    // }
 }
--- 3D Cellular Automata/Assets/_Game/_Scripts/GameBoard.cs
+++ 3D Cellular Automata/Assets/_Game/_Scripts/GameBoard.cs
@@ -9,6 +9,7 @@ using Random = UnityEngine.Random;
 
 public class GameBoard : MonoBehaviour
 {
+    public ComputeShader logicShader;
     public int3 boardDimensions = new int3(10, 10, 10);
     public int rule = 1;
     public float cellSize = 1;
@@ -16,25 +17,28 @@ public class GameBoard : MonoBehaviour
     public float timeBetweenGenerations = 1f;
     [Range(0f, 1f)] public float deadToAliveRatio = 0.75f; 
     public GameObject cube;
-    public Dictionary<int3, Cell> currentCellArray;
-    public Dictionary<int3, Cell> nextCellArray;
+    public Dictionary<int3, Cell> cellDictionary;
+    int[] aliveArray;
     Cellular_Logic cellular_Logic;
  
-
+    ComputeBuffer nextCellBuffer, currentCellBuffer;
     private void Start() 
     {
         cellular_Logic = GetComponent<Cellular_Logic>();
 
         cellCount = boardDimensions.x * boardDimensions.y * boardDimensions.z;
-        currentCellArray = new Dictionary<int3, Cell>();
-        nextCellArray = new Dictionary<int3, Cell>();
-        for (int x = 0; x < boardDimensions.x; x++){
+        cellDictionary = new Dictionary<int3, Cell>();
+        aliveArray = new int[cellCount];
+        int i = 0;
+        for (int z = 0; z < boardDimensions.z; z++){
             for (int y = 0; y < boardDimensions.y; y++){
-                for (int z = 0; z < boardDimensions.z; z++)
+                for (int x = 0; x < boardDimensions.x; x++)
                 {
                     Cell newCell = new Cell();
                     newCell.isAlive = (Random.Range(0.0f, 1.0f) > deadToAliveRatio) ? true : false;
-                    currentCellArray[new int3(x,y,z)] = newCell;
+                    cellDictionary[new int3(x,y,z)] = newCell;
+                    aliveArray[i] = newCell.isAlive ? 1 : 0;
+                    i++;
                 }
             }
         }
@@ -42,24 +46,23 @@ public class GameBoard : MonoBehaviour
         StartCoroutine(AdvanceGenerations());
     }
 
-
-    void Update()
-    {
-        
-    }
-
     private IEnumerator AdvanceGenerations()
     {
+        int3[] keyArray = new int3[cellDictionary.Count];
+        cellDictionary.Keys.CopyTo(keyArray, 0);
+
         while (true)
         {
-            cellular_Logic.CalculateNextEvolution();
+            CalculateNextGeneration();
 
-            int3[] keyArray = new int3[currentCellArray.Count];
-            currentCellArray.Keys.CopyTo(keyArray, 0);
-            for(int i = 0; i < currentCellArray.Count; i++)
+            for(int i = 0; i < cellDictionary.Count; i++)
             {
-                Destroy(currentCellArray[keyArray[i]].cellObject);
-                currentCellArray[keyArray[i]] = nextCellArray[keyArray[i]];
+                Destroy(cellDictionary[keyArray[i]].cellObject);
+
+                var k = cellDictionary[keyArray[i]];
+                k.isAlive = (aliveArray[i] == 1) ? true : false;
+                cellDictionary[keyArray[i]] = k;
+
                 SpawnCubes(keyArray[i]);
             }
 
@@ -67,21 +70,43 @@ public class GameBoard : MonoBehaviour
         }
     }
 
+    private void CalculateNextGeneration()
+    {
+        nextCellBuffer = new ComputeBuffer(cellCount, sizeof(int));
+        currentCellBuffer = new ComputeBuffer(cellCount, sizeof(int));
+        currentCellBuffer.SetData(aliveArray);
+
+        logicShader.SetBuffer(0, "currentCellBuffer", currentCellBuffer);
+        logicShader.SetBuffer(0, "nextCellBuffer", nextCellBuffer);
+        logicShader.SetInt("ruleToUse", rule);
+        logicShader.SetVector("boardDimensions", new float4(boardDimensions, 0));
+        
+        logicShader.Dispatch(0, Mathf.CeilToInt(boardDimensions.x / 8f), Mathf.CeilToInt(boardDimensions.y / 8f), Mathf.CeilToInt(boardDimensions.z / 8f));
+
+        nextCellBuffer.GetData(aliveArray);
+
+        currentCellBuffer.Release();
+        nextCellBuffer.Release();
+    }
+
     private void SpawnCubes(int3 centerIndex)
     {
-        if(currentCellArray[centerIndex].isAlive) // Cell is alive
+        if(cellDictionary[centerIndex].isAlive) // Cell is alive
         {
             Cell newCell = new Cell();
             newCell.cellObject = GameObject.Instantiate(cube);
+            newCell.cellObject.transform.parent = transform;
             newCell.cellObject.transform.localScale = (float3)cellSize;
             newCell.cellObject.transform.position = (float3)centerIndex * cellSize;
-            newCell.isAlive = currentCellArray[centerIndex].isAlive;
+            newCell.isAlive = cellDictionary[centerIndex].isAlive;
 
-            currentCellArray[centerIndex] = newCell;
+            cellDictionary[centerIndex] = newCell;
+
+            // Debug.Log("Alive");
         }
         else // cell is dead
         {
-
+            // Debug.Log("Dead");
         }
     }
 
